14d196536c4ef618ca31b2cbe890d69b1c2b4419
lukekrtzr <lkskratzer@gmail.com>
2026-05-13 07:55:36 +0000

    fixed heading

--- portfolio-site/src/App.vue
+++ portfolio-site/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <h1>{{ This is my Protfolio }}</h1>
+      <h1>This is my Portfolio</h1>
     </div>
   </header>
 
